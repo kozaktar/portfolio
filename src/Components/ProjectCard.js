30de7367138a5a0d71e31f7029a082768ab3959a
Taras <kozaktaras15@gmail.com>
2020-07-22 14:49:58 -0400

    imoroved mobile responsivness

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -28,7 +28,7 @@ const theme = createMuiTheme({
 const useStyles = makeStyles({
     root: {
       maxWidth:600 ,
-      height:620,
+      height:650,
       marginLeft:10,
       marginRight:10,
       position:'relative'
